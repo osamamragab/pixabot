20670419890c4bc0130aff8a04c3b26c5123bb61
Osama <osamaeletch@gmail.com>
2020-04-13 18:26:44 +0200

    add instagram

--- utils/push.js
+++ utils/push.js
@@ -1,11 +1,11 @@
 const twitter = require('../services/twitter');
 const telegram = require('../services/telegram');
-// const instagram = require('../services/instagram');
+const instagram = require('../services/instagram');
 
 module.exports = async (picPath, caption) => {
   await twitter(picPath, caption);
   await telegram(picPath, caption);
-  // await instagram(picPath, caption);
+  await instagram(picPath, caption);
 
   console.log('pushed to all');
 };
