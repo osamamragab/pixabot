76b2aa87bfd64a319175782f39bea4c7843a35d2
Osama <iosamaify@gmail.com>
2020-05-03 22:38:29 +0200

    rewrite unsplash with TypeScript

---
+++ src/services/unsplash.ts
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import { UNSPLASH_ACCESS_KEY } from '../utils/config';
+import type { PixaPic } from '../utils/getPic';
+
+interface UnsplashAPI {
+  id: string;
+  width: number;
+  height: number;
+  urls: {
+    raw: string;
+    full: string;
+    regular: string;
+    small: string;
+    thumb: string;
+  };
+  user: {
+    id: string;
+    name: string;
+    links: {
+      html: string;
+    };
+  };
+}
+
+const randomPhotoURL: string = 'https://api.unsplash.com/photos/random';
+
+export default async (): Promise<PixaPic> => {
+  const { data: pic } = await axios.get<UnsplashAPI>(randomPhotoURL, {
+    headers: {
+      Authorization: `Client-ID ${UNSPLASH_ACCESS_KEY}`
+    },
+    params: {
+      featured: true
+    }
+  });
+
+  return {
+    id: pic.id,
+    width: pic.width,
+    height: pic.height,
+    platform: 'Unsplash',
+    urls: {
+      original: pic.urls.full,
+      medium: pic.urls.regular,
+      small: pic.urls.small
+    },
+    author: {
+      id: pic.user.id,
+      name: pic.user.name
+    }
+  };
+};
