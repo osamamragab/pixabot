8197fec4c713a2055bfae442166eea8d6894115c
Osama <iosamaify@gmail.com>
2020-05-06 21:24:13 +0200

    add twitter client

---
+++ src/clients/twitter.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import FormData from 'form-data';
+import {
+  TWITTER_CONSUMER_KEY,
+  TWITTER_CONSUMER_SECRET,
+  TWITTER_ACCESS_TOKEN,
+  TWITTER_ACCESS_TOKEN_SECRET
+} from '../utils/config';
+
+export default async (buffer: Buffer, caption: string): Promise<void> => {};
