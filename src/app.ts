9cd0e2b1c59c585d86c0613a718ea48bc1874dac
Osama <iosamaify@gmail.com>
2020-05-06 21:21:44 +0200

    rewrite main file with TypeScript

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import getPic, { PixaPic } from './utils/getPic';
+import download from './utils/download';
+import publish from './utils/publish';
+
+// wrap everything
+async function main(): Promise<void> {
+  try {
+    // get the random pic
+    const pic: PixaPic = await getPic();
+
+    // download pic as buffer
+    const picBuffer: Buffer = await download(pic.urls.medium);
+
+    console.log(`(${pic.id}) downloaded successfully`);
+
+    // set caption
+    const caption: string = `by: ${pic.author.name.trim()} on #${pic.platform}`;
+
+    // publish the pic to all platforms
+    await publish(picBuffer, caption);
+
+    console.log(`(${pic.id}) was removed`);
+  } catch (err) {
+    console.error(err.stack || err.message);
+  }
+}
+
+// first run
+main();
+
+// run every day
+setInterval(main, 60 * 1000);
+// setInterval(main, 24 * 60 * 60 * 1000);
