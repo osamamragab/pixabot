ed25e2997e512317c570cb241b24cbdfb34dcefd
Osama <iosamaify@gmail.com>
2020-05-11 20:55:53 +0200

    fix telegram client issues

--- src/clients/telegram.ts
+++ src/clients/telegram.ts
@@ -1,35 +1,15 @@
-import axios from 'axios';
-import FormData from 'form-data';
+import TelegramBot from 'node-telegram-bot-api';
 
 const { TELEGRAM_TOKEN, TELEGRAM_CHAT } = process.env;
 
-interface TelegramAPIMessage {
-  message_id: number;
-  chat: {
-    id: number;
-    title: string;
-    username: string;
-    invite_link: string;
-    type: 'private' | 'group' | 'supergroup' | 'channel';
-  };
-}
-
-const sendPhotoURL: string = `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendPhoto`;
+const tg = new TelegramBot(TELEGRAM_TOKEN, {
+  polling: true
+});
 
 export default async (buffer: Buffer, caption: string): Promise<void> => {
-  const form = new FormData();
-
-  form.append('chat_id', TELEGRAM_CHAT);
-  form.append('caption', caption);
-  form.append('photo', buffer);
-
-  const { data: msg } = await axios.post<TelegramAPIMessage>(
-    sendPhotoURL,
-    form,
-    {
-      headers: form.getHeaders()
-    }
-  );
+  const msg = await tg.sendPhoto(TELEGRAM_CHAT, buffer, {
+    caption
+  });
 
   console.log(
     `(telegram): https://t.me/${msg.chat.username}/${msg.message_id}`
