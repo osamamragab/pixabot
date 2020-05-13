ac3b2ebe7832768c0dcfcdbed6743df24531f65a
Osama <iosamaify@gmail.com>
2020-05-14 01:00:58 +0200

    improve code readabilty

--- src/services/unsplash.ts
+++ src/services/unsplash.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import type { PixaPic } from '../utils/getPic';
 
 interface UnsplashAPI {
@@ -25,27 +25,28 @@ const { UNSPLASH_ACCESS_KEY } = process.env;
 
 const randomPhotoURL: string = 'https://api.unsplash.com/photos/random';
 
+const requestConfig: AxiosRequestConfig = {
+  headers: {
+    Authorization: `Client-ID ${UNSPLASH_ACCESS_KEY}`
+  },
+  params: {
+    featured: true,
+    orientation: 'landscape'
+  }
+};
+
 export default async (): Promise<PixaPic> => {
-  const { data: pic } = await axios.get<UnsplashAPI>(randomPhotoURL, {
-    headers: {
-      Authorization: `Client-ID ${UNSPLASH_ACCESS_KEY}`
-    },
-    params: {
-      featured: true,
-      orientation: 'landscape'
-    }
-  });
+  const { data: pic } = await axios.get<UnsplashAPI>(
+    randomPhotoURL,
+    requestConfig
+  );
 
   return {
     id: pic.id,
     width: pic.width,
     height: pic.height,
     platform: 'Unsplash',
-    urls: {
-      original: pic.urls.full,
-      medium: pic.urls.regular,
-      small: pic.urls.small
-    },
+    dl: pic.urls.regular,
     author: {
       id: pic.user.id,
       name: pic.user.name
